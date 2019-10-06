9eea18aead8ce3218650e4b0a38e1e92a806156b
asmarokadiya <asmarokadiya7@gmail.com>
2019-10-06 17:13:04 -0400

    Final Commit

---
+++ app/src/main/java/com/example/databaseexample/DatabaseHelper.java
@@ -0,0 +1,91 @@
+package com.example.databaseexample;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME="inventory";
+    public static final String DB_VERSION="1";
+    public static final String TABLE_NAME="product";
+
+    public static final String COL_1="ID";
+    public static final String COL_2="NAME";
+    public static final String COL_3="BRAND";
+    public static final String COL_4="COST";
+    public static final String COL_5="QTY";
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, null, Integer.parseInt(DB_VERSION));
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String productlist="Create table product(id integer primary key autoincrement, " +
+                "name varchar,brand varchar, cost number, qty number);";
+        db.execSQL(productlist);
+    }
+
+    public boolean addItems(String name, String brand, String cost,String qty){
+
+        SQLiteDatabase db=getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put("name",name);
+        contentValues.put("brand",brand);
+        contentValues.put("cost",cost);
+        contentValues.put("qty",qty);
+
+        db.insert("product",null,contentValues);
+        db.close();
+        return true;
+
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String productList="drop table if exists product";
+        db.execSQL(productList);
+        onCreate(db);
+    }
+
+    public boolean updateItems(String id, String name, String brand,String cost, String qty){
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,brand);
+        contentValues.put(COL_4,cost);
+        contentValues.put(COL_5,qty);
+
+
+        db.update(TABLE_NAME,contentValues,"ID=?",new String[] {id});
+        db.close();
+        return true;
+
+    }
+
+    public Integer deleteItems(String id){
+
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        return db.delete(TABLE_NAME, "ID=?", new String[] {id});
+    }
+
+    public Cursor viewAllItems(){
+        SQLiteDatabase db= this.getReadableDatabase();
+        Cursor res=db.rawQuery("select * from " + TABLE_NAME,null);
+        return res;
+
+    }
+
+  }
--- app/src/main/java/com/example/databaseexample/MainActivity.java
+++ app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -1,14 +1,129 @@
 package com.example.databaseexample;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText text1,text2,text3,text4,text5;
+    Button btnadd,btnupdate,btndelete,btnviewall;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=(EditText) findViewById(R.id.editText);
+        text2=(EditText) findViewById(R.id.editText2);
+        text3=(EditText) findViewById(R.id.editText3);
+        text4=(EditText) findViewById(R.id.editText4);
+        text5=(EditText) findViewById(R.id.editText5);
+
+
+        btnadd=(Button)findViewById(R.id.button);
+        btnupdate=(Button)findViewById(R.id.button2);
+        btndelete=(Button)findViewById(R.id.button3);
+        btnviewall=(Button)findViewById(R.id.button4);
+
+        db=new DatabaseHelper(this,"",null,0);
+        add_product();
+        update_product();
+        delete_product();
+        view_product();
     }
+    private void add_product(){
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean is_inserted=db.addItems(text2.getText().toString(),text3.getText().toString(),text4.getText().toString(),text5.getText().toString());
+                if(is_inserted){
+
+                    Toast.makeText(getApplicationContext(),"Successfully Inserted", Toast.LENGTH_LONG).show();
+                }else{
+
+                    Toast.makeText(getApplicationContext(),"Successfully  Not Inserted", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private void update_product(){
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean is_updated=db.updateItems(text1.getText().toString(), text2.getText().toString(),text3.getText().toString(),text4.getText().toString(),text5.getText().toString());
+                if(is_updated){
+
+                    Toast.makeText(getApplicationContext(),"Successfully updated", Toast.LENGTH_LONG).show();
+                }else{
+
+                    Toast.makeText(getApplicationContext(),"Successfully  Not updated", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private void delete_product(){
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer is_deleted = db.deleteItems(text1.getText().toString());
+                if(is_deleted>0){
+                    Toast.makeText(getApplicationContext(),"Successfully deleted", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Successfully not deleted", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void view_product(){
+        btnviewall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor red=db.viewAllItems();
+                if(red.getCount()==0){
+                    showMessage("error","No products found");
+                    return;
+                }
+
+                StringBuffer buffer=new StringBuffer();
+                while (red.moveToNext()){
+                    buffer.append("id, "+red.getString(0)+"\n");
+                    buffer.append("name,"+red.getString(1)+"\n");
+                    buffer.append("brand,"+red.getString(2)+"\n");
+                    buffer.append("cost,"+red.getString(3)+"\n");
+                    buffer.append("qty,"+red.getString(4)+"\n");
+
+
+                }
+
+                showMessage("data",buffer.toString());
+            }
+        });
+    }
+
+    private void showMessage(String error, String no_products_found) {
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(error);
+        builder.setMessage(no_products_found);
+        builder.show();
+    }
+
+
 }
